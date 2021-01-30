b0f4ba381c44b5f4a1ff1f69f9d1e523dc0f817c
Lucie Roland <lucieemroland@gmail.com>
2021-01-30 14:17:51 +1100

    mister cocktail last update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,6 +15,6 @@ class CocktailsController < ApplicationController
   end
   private
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   validates_uniqueness_of :name
    has_many :doses, dependent: :destroy
    has_many :ingredients, through: :doses
+   has_one_attached :photo
 end
